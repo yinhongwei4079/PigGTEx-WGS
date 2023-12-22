b95c8bd6af96c954a9a33a66b64d26d0085f8a34
yinhongwei4079 <511869835@qq.com>
2023-12-22 16:06:42 +0800

    Update generate_tf_select.sh

--- generate_tf_select.sh
+++ generate_tf_select.sh
@@ -1,4 +1,4 @@
-for i in Adipose Cecum Cerebellum Colon Cortex Duodenum Hypothalamus Ileum Jejunum Liver Lung Muscle Spleen Stomach
+for i in Adipose Cecum Colon Cortex Duodenum Hypothalamus Ileum Jejunum Liver Lung Muscle Spleen Stomach
 do
 bed_file=/home/yinhongwei/pig_yin/merge_peak/ATAC_${i}_Peaks.merge100.bed
 findMotifsGenome.pl ${bed_file} /home/yinhongwei/pig/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa ${i}_motif -len 8,10,12 -p 30
@@ -8,4 +8,15 @@ do
 knowfie=`basename $j|cut -d . -f 1`    
 scanMotifGenomeWide.pl $j /home/yinhongwei/pig/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa -p 30 > ${i}_tf/${i}.${knowfie}.tf_sites
 done
+done
+
+for i in Adipose Cecum Colon Cortex Duodenum Hypothalamus Ileum Jejunum Liver Lung Muscle Spleen Stomach
+do
+for j in ${i}_tf/${i}.motif*.tf_sites
+do
+awk -F '\t' '{if($6>=5.0){print $3"\t"$4"\t"$6"\t"$2}}' ${j}|awk '{print $4"\t"$1"\t"$2"\t"$3}' >>${i}.motif.bed
+done
+bed_file=/home/yinhongwei/pig_yin/merge_peak/ATAC_${i}_Peaks.merge100.bed
+bedtools intersect -a ${i}.motif.bed -b ${bed_file} -f 1.0 -wa > ${i}.OR5.motif.in.OCR
+rm ${i}.motif.bed
 done
